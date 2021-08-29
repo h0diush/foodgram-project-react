a84dc05d7af0be5111d0b810d5d11b69a4d79062
h0diush <>
2021-08-29 22:19:58 +0300

    flake8

--- backend/recipe/api/serializers.py
+++ backend/recipe/api/serializers.py
@@ -95,9 +95,9 @@ class RecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipe
         fields = ('id', 'tags', 'author', 'ingredients', 'is_favorited',
-                  'is_in_shopping_cart', 'name', 'image', 
+                  'is_in_shopping_cart', 'name', 'image',
                   'text', 'cooking_time'
-        )
+                  )
 
     def favorited(self, obj):
         request = self.context.get('request')
